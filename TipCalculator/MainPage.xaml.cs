caf9f63454c6bf6455e24016ebb4aa51fe88fe12
Isabelly Xavier <isabellyxavier1323@hotmail.com>
2024-08-23 20:51:03 -0300

    Fiz a interface

---
+++ TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SetUpTipButtom_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void SetDownTipButtom_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void RoundUpButtom_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void RoundDownButtom_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+    }
+
+}
