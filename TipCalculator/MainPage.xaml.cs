3e4a903869e83d79196d5ad954d1b123c525a5f4
Isabelly Xavier <isabellyxavier1323@hotmail.com>
2024-08-23 22:45:13 -0300

    Atualização

--- TipCalculator/MainPage.xaml.cs
+++ TipCalculator/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace TipCalculator
+﻿using Microsoft.Maui;
+
+namespace TipCalculator
 {
     public partial class MainPage : ContentPage
     {
@@ -9,24 +11,46 @@
             InitializeComponent();
         }
 
-        private void SetUpTipButtom_Clicked(System.Object sender, System.EventArgs e)
+        private void SetHighTipButtom_Clicked(System.Object sender, System.EventArgs e)
         {
-
+            TipSlider.Value = 20;
+            PercentLabel.Text = TipSlider.Value.ToString("#.##") + "%";
         }
 
         private void SetDownTipButtom_Clicked(System.Object sender, System.EventArgs e)
         {
-
+            TipSlider.Value = 15;
+            PercentLabel.Text = TipSlider.Value.ToString("#.##") + "%";
         }
 
         private void RoundUpButtom_Clicked(System.Object sender, System.EventArgs e)
         {
-
+            TipSlider.Value = ((int)TipSlider.Value) + 1;
         }
 
         private void RoundDownButtom_Clicked(System.Object sender, System.EventArgs e)
         {
+            TipSlider.Value = ((int)TipSlider.Value) - 1;
+        }
 
+        private void TipSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+ 
+            PercentLabel.Text = TipSlider.Value.ToString("#.##")+"%";
+
+            double valor = Convert.ToDouble(AmountEntry.Text);
+            double valorGorjeta = valor * (TipSlider.Value / 100);
+            double valorTotal = valorGorjeta + valor;
+
+            TipLabel.Text = valorGorjeta.ToString();
+            TotalLabel.Text = valorTotal.ToString();
+
+        }
+
+
+        private void AmountEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
         }
     }
 
